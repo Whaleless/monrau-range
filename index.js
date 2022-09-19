6972eaf242a1f3f9a7299ec0e058dfac0ebc027f
Whaleless <97296345+Whaleless@users.noreply.github.com>
2022-09-19 22:48:05 +0300

    audioFinalFinal

--- index.js
+++ index.js
@@ -81,7 +81,6 @@ function sleep(ms) {
 }
 
 launchScreen.addEventListener('click', async function () {
-    backAudio.src = './back.mp3';
     backAudio.loop = true;
     backAudio.muted = true;
     backAudio.play();
